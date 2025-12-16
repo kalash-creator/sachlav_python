485058139e9787f9a3a4f7148f4ec28125836879
kalash-creator <kalashtan87@gmail.com>
2025-12-16 19:01:27 +0200

    adding main py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 print("Hello World")
 print("Hello World")
 print("Hello World")
+print("Tatiana")
+print("Kalashnikov")
+print(38)
+print("Tatiana Kalashnikov - age 37")
+name = "Tatiana"
+main_age = 38
+print(name)
+print(main_age)
+print(main_age)
+print(type(main_age))
+
+main_age = "age"
+print(main_age)
+print(main_age)
+print(main_age)
+print(type(main_age))
+"""
+The main comment"""
+d=4/8
+print(type(d))
+a=5
+print(type(a))
+b=8-4
+print(type(b))
+c=3*3
+print(type(c))
