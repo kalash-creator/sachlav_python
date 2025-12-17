81f6fb6cf8ce6e7f725b756fe22dd027c6aaa79a
kalash-creator <kalashtan87@gmail.com>
2025-12-17 17:46:26 +0200

    second commit

--- arithmetic_functions.py
+++ arithmetic_functions.py
@@ -1,3 +1,10 @@
 print(-7.25)
 print(abs(-7.25))
 print(abs(7.25))
+
+print(max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+print(min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+print(round(2.4945645645))
+print(round(2.35434564, 2))
+print(max(1, -2, 49, 1001, abs(-2550)))
