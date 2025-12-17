6c07aac307e66f971b770c573f11b64d4bdcb300
kalash-creator <kalashtan87@gmail.com>
2025-12-17 18:54:43 +0200

    fixing structure

---
+++ numbers.py
@@ -0,0 +1,11 @@
+
+
+a=23
+b=21
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
--- variables.py
+++ variables.py
@@ -1,1 +1,35 @@
-f
+a = 5
+name = "Alex"
+full_name = "Tatiana Kalashnikov"
+print(full_name)
+age = 38
+print(age)
+age = 678
+print(age)
+
+# how to begin variables
+
+A_b = "b"
+abc = "abc"
+ABC = "abc"
+r1_2_r = 56
+_fd = "b"
+#  ####not valid prefix
+#  $_dfg="v"
+# 3_b=5
+# r_$dfg=56
+#it is a string - not a comment
+"""fgfhfdfhfgh
+fhfghfgh
+dfhgfghfh
+fghfgh"""
+
+
+arr_1= [1,2,3,3,4]
+set_1={1,2,3,3,3,4}
+print(arr_1)
+print(set_1)
+
+arr_1=set(arr_1)
+arr_1=list(arr_1)
+
