2437cd5c47ab46084b9cd84caa7c15597dd80a35
kalash-creator <kalashtan87@gmail.com>
2025-12-23 20:26:07 +0200

    adding lesson 3 materials examples

---
+++ lesson_3/booleans.py
@@ -0,0 +1,32 @@
+# is_true= True
+# is_false= False
+# print(is_true, type(is_true))
+# print(is_false, type(is_false))
+
+# print(bool(1))
+# print(bool(0))
+# print(bool(100))
+# print(bool(-1))
+
+
+# print(bool("A"))
+# print(bool(""))
+# print(bool(" "))
+# print(bool(None))
+
+# print(5==5)
+# print(5==6)
+# print(5!=5)
+# print(5!=6)
+# print(5>=5)
+# print(6>=5)
+# print(4>=5)
+#
+# print(5>4)
+# print(5>5)
+#
+# print(5<5)
+# print(4<5)
+
+print(4%2==0)
+print(4%3==0)
---
+++ lesson_3/booleans2.py
@@ -0,0 +1,24 @@
+# print(5>4 and 5>3)
+# print(5>4 and 5<3)
+# print(5<4 and 5>3)
+# print(5<4 and 5<3)
+# print(5>4 or 5<3 and True)
+# print(5>4 and 5>3 and True)
+
+# print(5>4 or 5>3)
+# print(5>4 or 5<3)
+# print(5<4 or 5>3)
+# print(5<4 or 5<3)
+# print(5>4 or 5<3 and True)
+# print(5>4 or 5>3 and True)
+
+print (not True)
+print (not False)
+
+
+print (False ^ True)
+print (True ^ False)
+print (False ^ False)
+print (True ^ True)
+
+print (True ^ False or True and False)
