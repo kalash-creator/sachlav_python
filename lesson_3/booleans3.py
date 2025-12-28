fb46e8c94ed67829f0193ab4fffb10459ab53375
kalash-creator <kalashtan87@gmail.com>
2025-12-28 17:53:50 +0200

    lesson 3

---
+++ lesson_3/booleans3.py
@@ -0,0 +1,27 @@
+# num=int(input('Enter a number: '))
+# print(num>0)
+# if num>0:
+#     print("True")
+#     else:
+#     print("False")
+
+num = int(input('Enter a number: '))
+print(num%9!=0)
+
+a, b,c=map(int,input('Enter 3 lengths: ').split())
+print(a==b==c)
+
+numb = int(input('Enter a number: '))
+print(5<numb<=19)
+
+
+
+x,y,z=map(int,input('Enter 3 lengths: ').split())
+print(a==b or b==c or c==a)
+
+
+d,v=map(int,input('Enter 2 numbers: ').split())
+print(d%7==0 and v%7==0)
+
+num2 = int(input('Enter a number: '))
+print(num2/10<10)
