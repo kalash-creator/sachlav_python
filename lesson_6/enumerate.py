956e37030a6138bc265dc1fd57a3005feab8b2e1
kalash-creator <kalashtan87@gmail.com>
2026-01-12 14:59:31 +0200

    task_6.1

---
+++ lesson_6/enumerate.py
@@ -0,0 +1,16 @@
+# word=["one","two","three","four","five","six","seven","eight","nine"]
+# for index, word in enumerate(word, start=1):
+#     print(f"LaLaLa {index}={word}")
+contacts=[]
+contacts["contact_1"]={
+"name": "Анна Иванова",
+"phone": "+79001234567",
+"email": "anna.ivanova@example.com"
+}
+contacts["contact_2"]={
+"name": "Петр Сидоров",
+"phone": "+79119876543",
+"email": "petr.sidorov@example.com"
+}
+
+
---
+++ lesson_6_tasks/task_6.1.py
@@ -0,0 +1,13 @@
+inventory = ['apple', 'banana', 'orange', 'grape', 'pineapple', 'kiwi']
+processed_items=[]
+char_line=('a', 'e', 'i', 'o', 'u')
+for index, item in enumerate(inventory, start=1):
+    if index%2==0 and len(item)>5:
+        processed_items.append(f'{index}. Удлиненный {item}')
+
+    elif index%2==1 and item[0] in char_line:
+        processed_items.append(f'{index}. Стартует с гласной: {item}')
+
+    else: processed_items.append(f'{index}. {item}')
+print(f"\nИсходный список: {inventory}")
+print(f"Обработанные элементы: \n{processed_items}")
